d7cf5dc26a0c79a73e08ff0260ce9ff871fbd2c5
pavandantu18 <pavandantu18@gmail.com>
2026-03-08 06:16:43 -0400

    added dist folder to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,7 @@ connectDB();
 // Middleware
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
+app.use(express.static("./public"))
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
